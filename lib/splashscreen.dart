aeb41f57c2a4bc29cd3b6078152d1bcb5d3f04b0
Vindo Rizkiyanto <48651819+vindorizkiyanto@users.noreply.github.com>
2021-07-20 20:36:47 +0700

    Add files via upload

---
+++ lib/splashscreen.dart
@@ -0,0 +1,55 @@
+import 'package:chatapp/pages/authentication/welcome_page.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class SplashScreen extends StatefulWidget {
+  _SplashScreen createState() => _SplashScreen();
+}
+
+class _SplashScreen extends State<SplashScreen> {
+  void initState() {
+    super.initState();
+    splashscreenStart();
+  }
+
+  splashscreenStart() async {
+    var duration = const Duration(seconds: 3);
+    return Timer(duration, () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => WelcomePage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Icon(
+              Icons.chat,
+              size: 100.0,
+              color: Colors.white,
+            ),
+            SizedBox(
+              height: 24.0,
+            ),
+            Text(
+              "Hello!",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 30.0,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
